780b7e968dc9158282a29caadfd841b107427dcb
nobukunisyota <nobukuni.syota0727@gmail.com>
2024-01-29 23:52:33 +0900

    modify main function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-document.addEventListener('DOMContentLoaded',getTextBoxValue)
-
-function getTextBoxValue() {
-    let context = document.getElementById('textbox_input');
-	context.addEventListener('keydown', enterKeyPress);
+function modify_api_data(data) {
 }
 
-function enterKeyPress(event){
-	if(event.key === 'Enter'){
-		console.log('Press your Enter key.')
-        console.log(event.target.value);
-	}
+function main() {
+    console.log('main function start');
 }
-
-function buttonClick() {
-    fetch('https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060')
-    .then(response => response.json())
-    .then((json) => {
-        let hoge = json;
-        console.log(hoge.results[0]);
-        let value = Object.values(hoge.results[0]);
-        console.log(value);
-    });
-}
+main();
