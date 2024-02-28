9ed8de12321fabdebd357f7799f3e1c5c5b3a92b
nobukunisyota <nobukuni.syota0727@gmail.com>
2024-02-28 22:30:02 +0900

    Update Frontend

--- api-test-frontend/src/App.tsx
+++ api-test-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css'
 import ApiGet from './component/ApiGet'
+import ApiGetByID from './component/ApiGetByID';
 
 function App() {
     // Get API All Tasks
@@ -8,10 +9,17 @@ function App() {
     const getApiHandler = () => {
         setApiGetEvent(!apiGetEvent);
     }
+    // Get API Tasks By ID
+    const [getTask, setGetTask] = useState(false);
+    const getTaskHandler = () => {
+        setGetTask(!getTask);
+    }
     return (
         <div>
             <button onClick={() => getApiHandler()}>Get All Task</button>
             {apiGetEvent ? <ApiGet /> : null}
+            <button onClick={() => getTaskHandler()}>Get All ID</button>
+            {getTask ? <ApiGetByID id_num="1"/> : null}
         </div>
     )
 }
---
+++ api-test-frontend/src/component/ApiGetByID.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ApiGetByID(props) {
+    return (
+        <div>{props.id_num}</div>
+    )
+}
+
+export default ApiGetByID
